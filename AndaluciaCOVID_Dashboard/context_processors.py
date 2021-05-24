3bd59c66da12851417c4420e3cd8969859a0cd31
carmen.morales <carmen.morales.bonet@gmail.com>
2021-05-24 20:07:38 +0200

    added new context processor

--- AndaluciaCOVID_Dashboard/context_processors.py
+++ AndaluciaCOVID_Dashboard/context_processors.py
@@ -2,3 +2,6 @@ from .models import *
 
 def getAllTerritories(request):
     return {'territories': Township.objects.all()} 
+
+def getLastUpdate(request):
+    return {'date': AcumulatedRegion.objects.all().order_by('-date',)[0].date}     
